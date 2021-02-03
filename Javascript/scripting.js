95e91bc42864d1b82fe71b3427dd072139685cbe
ShyamFaguna <52468359+ShyamFaguna@users.noreply.github.com>
2021-02-03 17:39:25 +0530

    Update scripting.js

--- Javascript/scripting.js
+++ Javascript/scripting.js
@@ -9,7 +9,6 @@ class2 {
 	branch:test,
 	node:js;
 }
-<<<<<<< HEAD
 class3 {
 	root:3-4,
 	branch:test,
@@ -30,5 +29,3 @@ class6 {
 	branch:test,
 	node:js..;
 }
-=======
->>>>>>> 40c98526109e1e170f5e953ec22249858845f1f8
